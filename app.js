291d0ecf774024a5f32a8191fd35b916225e91fc
Graschatium <legraschat.officiel@gmail.com>
2025-02-02 00:27:39 +0100

    Update app.js

--- app.js
+++ app.js
@@ -153,7 +153,7 @@ function updateCountdown() {
     }
 
     const diff = targetTime - now;
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
     const hours = Math.floor(diff / (1000 * 60 * 60));
     const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((diff % (1000 * 60)) / 1000);
