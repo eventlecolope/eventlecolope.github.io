283ca39fda670437aaa8e12dff0f6f2c3a64c4b4
Graschatium <legraschat.officiel@gmail.com>
2025-02-01 12:34:54 +0100

    modify site

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-// Exemple de données pour les participants et leurs points
+// Exemple de données pour les participants et leurs points, avec leurs IDs Discord et avatars
 const participants = [
-    { name: 'Personne', points: 0 }
+    { name: '🎯MrTraxy🎮', points: 0, discordId: '1078363268185346100', avatar: 'd74f819866fcc276979d97dc77651e10' },
+    { name: 'Predator 🗿    ', points: 0, discordId: '1282764464822685707', avatar: '2c89ec4bc293e1e5b0ec14cbabcece2a' },
 
 ];
 
@@ -16,8 +17,16 @@ function displayLeaderboard() {
         const row = document.createElement('tr');
         row.innerHTML = `
             <td>${index + 1}</td>
-            <td>${participant.name}</td>
+            <td>
+                <a href="https://discord.com/users/${participant.discordId}" target="_blank">
+                    <img src="https://cdn.discordapp.com/avatars/${participant.discordId}/${participant.avatar}.webp?size=128" alt="Avatar de ${participant.name}" class="avatar">
+                    ${participant.name}
+                </a>
+            </td>
             <td>${participant.points}</td>
+            <td>
+                <a href="https://discord.com/users/${participant.discordId}" target="_blank">Voir Profil</a>
+            </td>
         `;
         leaderboard.appendChild(row);
     });
